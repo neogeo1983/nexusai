afc608d905ecf47dce4351883bc64e5b7345efd0
neogeo1983 <ccl231693@cecos.ac.uk>
2026-08-02 17:50:40 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,181 +1,57 @@
-"use client";
+import { NextResponse } from "next/server";
 
-import {useEffect,useState} from "react";
+export async function GET() {
 
+  const url = process.env.IPTV_LIST_1;
 
-type Channel={
-name:string;
-group:string;
-url:string;
-};
 
+  if (!url) {
+    return new NextResponse(
+      "Playlist missing",
+      { status: 500 }
+    );
+  }
 
 
-export default function Channels(){
+  try {
 
+    const response = await fetch(url, {
+      cache: "no-store"
+    });
 
-const [channels,setChannels]=useState<Channel[]>([]);
-const [selected,setSelected]=useState<Channel|null>(null);
 
+    if (!response.ok) {
 
+      return new NextResponse(
+        "Playlist error",
+        { status: 500 }
+      );
 
-useEffect(()=>{
+    }
 
-fetch("/api/playlist")
-.then(res=>res.text())
-.then(data=>{
 
+    const playlist = await response.text();
 
-const lines=data.split("\n");
 
-let result:Channel[]=[];
+    return new NextResponse(
+      playlist,
+      {
+        headers:{
+          "Content-Type":"text/plain"
+        }
+      }
+    );
 
 
-for(let i=0;i<lines.length;i++){
+  } catch(error){
 
-if(lines[i].startsWith("#EXTINF")){
 
+    return new NextResponse(
+      "Connection failed",
+      { status:500 }
+    );
 
-const info=lines[i];
 
-const name=
-info.split(",")[1] || "Unknown";
-
-
-const groupMatch=
-info.match(/group-title="([^"]+)/);
-
-
-const group=
-groupMatch ? groupMatch[1]:"Other";
-
-
-const url=
-lines[i+1];
-
-
-result.push({
-name,
-group,
-url
-});
-
-
-}
-
-}
-
-
-setChannels(result);
-
-
-if(result.length>0)
-setSelected(result[0]);
-
-
-});
-
-
-},[]);
-
-
-
-return(
-
-<main className="min-h-screen bg-black text-white p-6">
-
-
-<h1 className="text-5xl font-bold mb-8">
-
-<span className="text-red-600">
-Neon
-</span>-IPTV
-
-</h1>
-
-
-
-<div className="grid lg:grid-cols-4 gap-6">
-
-
-
-<div className="bg-gray-900 rounded-3xl p-5">
-
-
-<h2 className="text-xl font-bold mb-5">
-Canale
-</h2>
-
-
-{channels.map((c,index)=>(
-
-
-<button
-
-key={index}
-
-onClick={()=>setSelected(c)}
-
-className="w-full text-left bg-gray-800 hover:bg-red-600 p-4 rounded-xl mb-3"
-
->
-
-📺 {c.name}
-
-</button>
-
-
-))}
-
-
-</div>
-
-
-
-
-<div className="lg:col-span-3 bg-gray-900 rounded-3xl p-5">
-
-
-{selected &&
-
-<video
-
-key={selected.url}
-
-src={selected.url}
-
-controls
-
-autoPlay
-
-className="w-full aspect-video bg-black rounded-xl"
-
-/>
-
-}
-
-
-
-{selected &&
-
-<h2 className="text-2xl mt-5 font-bold">
-
-{selected.name}
-
-</h2>
-
-}
-
-
-</div>
-
-
-</div>
-
-
-</main>
-
-
-)
+  }
 
 }
